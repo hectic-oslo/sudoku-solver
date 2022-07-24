077500de83f5de90ad2597ceec05cb522be2ffca
Abhishek Kumar <80272610+hectic-oslo@users.noreply.github.com>
2022-07-24 19:48:28 +0530

    update

---
+++ src.js
@@ -0,0 +1,32 @@
+const path = require('path')
+var hbs = require('hbs')
+const express = require('express')
+const app = express()
+ 
+
+
+
+//define path for express config.
+const publicDirectoryPath = path.join(__dirname, './public')
+ 
+
+//setup handlebars engine 
+   app.set('view engine','hbs')
+    
+
+ const PORT = process.env.PORT || 3000;
+
+
+//  //setup static directory to serve
+  app.use(express.static(publicDirectoryPath))
+ 
+
+app.get('',  (req, res)=>{
+  res.render('index',{
+     name:"Abhishek Kumar"
+  })
+})
+
+ 
+
+app.listen(PORT,()=>{console.log("server started at port "+PORT)})
