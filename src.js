30d154fc256585edb9faf971cf0d3c4cb6e2a3ef
Abhishek Kumar <80272610+hectic-oslo@users.noreply.github.com>
2022-07-24 19:56:26 +0530

    ok

--- src.js
+++ src.js
@@ -21,7 +21,7 @@ const publicDirectoryPath = path.join(__dirname, './public')
   app.use(express.static(publicDirectoryPath))
  
 
-app.get('',  (req, res)=>{
+app.get('/',  (req, res)=>{
   res.render('index',{
      name:"Abhishek Kumar"
   })
